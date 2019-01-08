788f39b5ce327e7d4637af5c5d65dd04845d7590
jason reid <jasonbrandon9406@gmail.com>
2019-01-08 00:08:25 +0000

    returned formatted date from helper function instead of full object of separate keys

--- src/classes/helper.js
+++ src/classes/helper.js
@@ -1,29 +1,32 @@
 export default class {
-	static isJSON(val){
-		try {
-			JSON.parse(val) 
-		} catch (e){			
-			console.log('not json');
-		}
+  static isJSON(val) {
+    try {
+      JSON.parse(val)
+    } catch (e) {
+      console.log('not json');
+    }
 
-		return 'is json';
-	}
+    return 'is json';
+  }
 
-	static randomString(){
-		return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-	}
+  static randomString() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
 
-	static formatDate(timestamp){
-		let date = new Date((timestamp)),
-			months = ['january','february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+  static formatDate(timestamp) {
+    let date = new Date((timestamp)),
+      months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
-		return {
-			day: date.getDate(),
-			month: months[date.getMonth()],
-			minutes: date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes(),
-			hours: date.getHours() < 10 ? `0${date.getHours()}` : date.getHours(),
-			year: date.getFullYear(),
-			prefix: date.getHours() < 12 ? 'am':'pm' 
-		}
-	}
+    let obj = {
+      day: date.getDate(),
+      month: months[date.getMonth()],
+      minutes: date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes(),
+      hours: date.getHours() < 10 ? `0${date.getHours()}` : date.getHours(),
+      year: date.getFullYear(),
+      prefix: date.getHours() < 12 ? 'am' : 'pm',
+    }
+    return {
+      readableDate: `${obj.day} ${obj.month} &bull; ${obj.hours}:${obj.minutes} ${obj.prefix} &bull; ${obj.year}`
+    }
+  }
 }
