850484e4d1e5bef84971da6b8cdcdc4e64896940
Jason Reid <jason.reid@appraisd.com>
2019-01-18 17:38:18 +0000

    added catch statement to init storage function

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
 import {
-  routes
+    routes
 } from './routes'
 import store from './store'
 import VueRouter from 'vue-router'
@@ -13,7 +13,7 @@ require('bootstrap');
 // require('localforage');
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 
@@ -23,63 +23,65 @@ Vue.use(VueRouter);
 /////
 
 Vue.filter('removeHash', (value) => {
-  let arr = hashword.split('');
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] === '#') {
-      arr.splice(0, 1);
+    let arr = hashword.split('');
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[0] === '#') {
+            arr.splice(0, 1);
+        }
     }
-  }
-  return arr.join('');
+    return arr.join('');
 });
 
 ////
 
 function storageAvailable(type) {
-  try {
-    var storage = window[type],
-      x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return e instanceof DOMException && (
-        // everything except Firefox
-        e.code === 22 ||
-        // Firefox
-        e.code === 1014 ||
-        // test name field too, because code might not be present
-        // everything except Firefox
-        e.name === 'QuotaExceededError' ||
-        // Firefox
-        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      storage.length !== 0;
-  }
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
 }
 
 router.beforeEach((to, from, next) => {
-  if (store.getters.appStatus) {
-    Storage.updateData().then(()=>{
-       next();
-    });
-    return;
-  }
+    if (store.getters.appStatus) {
+        Storage.updateData().then(() => {
+            next();
+        }).catch((err) => {
+            next();
+        });
+        return;
+    }
 
-  // initial load when app is functional
-  if (!store.getters.appStatus) {
-    // init cat or notes storage
-    Storage.initStorage().then((res) => {
-      next();
-    }).catch((err) => {
-      alert('there was an issue');
-    });
-  }
+    // initial load when app is functional
+    if (!store.getters.appStatus) {
+        // init cat or notes storage
+        Storage.initStorage().then((res) => {
+            next();
+        }).catch((err) => {
+            alert('there was an issue');
+        });
+    }
 
 })
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
