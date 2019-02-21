aac7d8b92e0485917cd3f149a87ec3cd9a24a391
Jason Reid <jason.reid@appraisd.com>
2019-02-21 18:02:11 +0000

    added create note functionalty from notes page when no notes exist

--- src/components/dashboard/notes/Notes.vue
+++ src/components/dashboard/notes/Notes.vue
@@ -11,24 +11,43 @@
       <!-- <div class="col-12 col-sm-6 col-lg-3" v-for="(note, i) in latestNotes" :key="i">
 		  <note-card :note="note" :category-belongs-to="getCategory(note.categoryID).cat"></note-card>
 	  </div> -->
-	  <div class="col-12">
-		  <div class="card-columns">
-			  <note-card :note="note" v-for="(note, i) in latestNotes" :key="i"></note-card>
-		  </div>
-	  </div>
+      <div class="col-12" v-if="latestNotes.length > 0">
+        <div class="card-columns">
+          <note-card :note="note" v-for="(note, i) in latestNotes" :key="i"></note-card>
+        </div>
+      </div>
+       <div class="col-12" v-else>
+          <div
+            class="create-component d-flex flex-column justify-content-center align-items-center hfx-400"
+          :data-action="'note'"
+          @click="triggerModal">
+            <div class="h3" >
+              <i class="fas fa-plus-circle hvr-grow" :style="{color: '#9b9b9b'}"></i>
+              <p>Create a note</p>
+            </div>
+          </div>
+        </div>
     </div>
+      <note-modal
+      :id="modalID('note')"
+      :all-categories-p="allCategories"
+    ></note-modal>
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
 import NoteCard from "./../widgets/Note.vue";
+import NoteModal from "./../../modals/CreateNoteModal.vue";
+import HelperMixin from "./../../../mixins/helpers.js";
 
 export default {
+  mixins:[HelperMixin],
   components: {
-    "note-card": NoteCard
+    "note-card": NoteCard,
+    "note-modal":NoteModal
   },
   computed: {
-    ...mapGetters(["allNotes", "getCategory"]),
+    ...mapGetters(["allNotes", "allCategories","getCategory", "modalID"]),
     latestNotes() {
       return this.allNotes.sort(function(a, b) {
         return b.createdAt - a.createdAt;
